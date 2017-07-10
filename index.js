46e98eaa228c8035123aa68c3421a24679339acd
John Burwell <john.burwell@crystaphase.com>
2017-07-09 21:11:39 -0500

    first working version
    first working version

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+'use strict';
+
 var Accessory, hap, UUIDGen;
 
+var http = require('http');
+var debug = require('debug')('camera-ffmpeg-ufv');
 var FFMPEG = require('./ffmpeg').FFMPEG;
 
 module.exports = function(homebridge) {
@@ -27,34 +31,158 @@ function ffmpegUfvPlatform(log, config, api) {
   }
 }
 
-  // Won't be invoked
+// Won't be invoked
 ffmpegUfvPlatform.prototype.configureAccessory = function(accessory) {
 }
 
 ffmpegUfvPlatform.prototype.didFinishLaunching = function() {
   var self = this;
 
-  if (self.config.cameras) {
+  if (self.config.nvrs) {
+
     var configuredAccessories = [];
 
-    var cameras = self.config.cameras;
-    cameras.forEach(function(cameraConfig) {
-      var cameraName = cameraConfig.name;
-      var videoConfig = cameraConfig.videoConfig;
+    var nvrs = self.config.nvrs;
 
-      if (!cameraName || !videoConfig) {
-        self.log("Missing parameters.");
-        return;
-      }
+    nvrs.forEach(function(nvrConfig) {
 
-      var uuid = UUIDGen.generate(cameraName);
-      var cameraAccessory = new Accessory(cameraName, uuid, hap.Accessory.Categories.CAMERA);
-      var cameraSource = new FFMPEG(hap, cameraConfig);
-      cameraAccessory.configureCameraSource(cameraSource);
-      configuredAccessories.push(cameraAccessory);
+      // From the config we need the host and API key for the NVR.
+      // - Host will be the NVR's hostname or IP and port, ie "nvr.example.com:7080"
+      // - API key is created in NVR user settings
+      var options = {
+        host: nvrConfig.apiHost,
+        port: nvrConfig.apiPort,
+        path: '/api/2.0/bootstrap?apiKey=' + nvrConfig.apiKey
+      };
+
+
+      // Fetch the "bootstrap" file from the NVR,
+      // which contains all the config info we need:
+
+      http.get(options, function (res) {
+
+        var json = '';
+
+        res.on('data', function (chunk) {
+          json += chunk;
+        });
+
+        res.on('end', function () {
+
+          if (res.statusCode === 200) {
+
+            try {
+              var parsedResponse = JSON.parse(json);
+
+              // At this point we should have the NVR configuration.
+
+              var serverName;
+              var streamingHost;
+              var streamingPort;
+              var channels = [];
+
+              // The root of the result is "data"
+              var discoveredNvrs = parsedResponse.data;
+
+              discoveredNvrs.forEach(function(discoveredNvr) {
+                debug("Discovered NVR " + discoveredNvr.nvrName);
+
+                // The NVR knows the rtsp port:
+                streamingPort = discoveredNvr.systemInfo.rtspPort;
+
+                // Within each NVR we should have one or more servers:
+                var discoveredServers = discoveredNvr.servers;
+
+                discoveredServers.forEach(function(discoveredServer) {
+                  debug("Discovered server " + discoveredServer.name);
+
+                  serverName = discoveredServer.name;
+
+                  // Hostname for the streams:
+                  streamingHost = discoveredServer.host;
+
+                });
+
+                // Hack: there is at this time only one 'server' object.
+                // We are assuming there will only be one.
+                // If this changes, things will probably break!
+
+                // Within each NVR we should have one or more cameras:
+                var discoveredCameras = discoveredNvr.cameras;
+
+                discoveredCameras.forEach(function(discoveredCamera) {
+                  // Each camera has more than one channel.
+                  // The channel is where the actual streaming params live:
+
+                  var discoveredChannels = discoveredCamera.channels;
+
+                  // Go through each channel, see if it is rtspEnabled, and if so,
+                  // post it to homebridge and move on to the next camera
+
+                  for(var channelIndex = 0; channelIndex < discoveredChannels.length; channelIndex++) {
+
+                    var discoveredChannel = discoveredChannels[channelIndex];
+
+                    // Let's see if this channel has RSTP enabled:
+                    if(discoveredChannel.isRtspEnabled == true) {
+                      var rtspAlias = discoveredChannel.rtspAlias;
+
+                      debug('Discovered RTSP enabled camera ' + discoveredCamera.uuid);
+
+                      // We should know have everything we need and can push it to
+                      // FFMPEG:
+
+                      var videoConfig = {
+                        "source": ("-re -i rtsp://" + streamingHost + ":" + streamingPort + "/" + rtspAlias + "?apiKey=" + nvrConfig.apiKey),
+                        "maxStreams": 2,
+                        "maxWidth": discoveredChannel.width, // or however we end up getting to this!
+                        "maxHeight": discoveredChannel.height,
+                        "maxFPS": discoveredChannel.fps
+                      };
+
+                      debug('Config: ' + JSON.stringify(videoConfig));
+
+                      // Create a new Accessory for this camera:
+                      var cameraAccessory = new Accessory(discoveredCamera.name, discoveredCamera.uuid, hap.Accessory.Categories.CAMERA);
+                      var cameraConfig = {name: discoveredCamera.name, videoConfig: videoConfig};
+
+                      debug(JSON.stringify(cameraConfig));
+
+                      var cameraSource = new FFMPEG(hap, cameraConfig);
+                      cameraAccessory.configureCameraSource(cameraSource);
+                      configuredAccessories.push(cameraAccessory);
+
+                      // Jump out of the loop once we have one:
+                      channelIndex = discoveredChannels.length;
+
+                    };
+
+                  };
+
+                });
+
+              });
+
+              // Publish the cameras we found to homebridge:
+              self.api.publishCameraAccessories("camera-ffmpeg-ufv", configuredAccessories);
+
+              self.log('Published ' + configuredAccessories.length + ' camera accessories.');
+
+            } catch (e) {
+              debug('Error parsing JSON! ' + e);
+            }
+
+          } else {
+            debug('Status:', res.statusCode);
+          }
+        });
+
+      }).on('error', function (err) {
+        debug('Error:', err);
+      });
+
     });
 
-    // Publish the cameras we found to homebridge:
-    self.api.publishCameraAccessories("Camera-ffmpeg-ufv", configuredAccessories);
   }
+
 }
