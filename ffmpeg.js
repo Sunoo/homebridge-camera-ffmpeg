5b06626810d4f7144db9431623bdbd841baa01fd
Tian Zhang <khaos.tian@gmail.com>
2018-02-09 21:57:24 -0800

    Override MSB for SSRC to ensure we get an valid Int32.

--- ffmpeg.js
+++ ffmpeg.js
@@ -164,7 +164,9 @@ FFMPEG.prototype.prepareStream = function(request, callback) {
     let srtp_salt = videoInfo["srtp_salt"];
 
     // SSRC is a 32 bit integer that is unique per stream
-    let ssrc = crypto.randomBytes(4).readInt32BE(0, true);
+    let ssrcSource = crypto.randomBytes(4);
+    ssrcSource[0] = 0;
+    let ssrc = ssrcSource.readInt32BE(0, true);
 
     let videoResp = {
       port: targetPort,
@@ -187,7 +189,9 @@ FFMPEG.prototype.prepareStream = function(request, callback) {
     let srtp_salt = audioInfo["srtp_salt"];
 
     // SSRC is a 32 bit integer that is unique per stream
-    let ssrc = crypto.randomBytes(4).readInt32BE(0, true);
+    let ssrcSource = crypto.randomBytes(4);
+    ssrcSource[0] = 0;
+    let ssrc = ssrcSource.readInt32BE(0, true);
 
     let audioResp = {
       port: targetPort,
