d298db8e3a50d0b01c2ce3309cecdae8e5b1cf97
Epalzeo Lithe <epalzeo.lithe@gmail.com>
2019-01-20 16:24:55 +0100

    Update ffmpeg.js

--- ffmpeg.js
+++ ffmpeg.js
@@ -144,11 +144,18 @@ FFMPEG.prototype.handleCloseConnection = function(connectionID) {
 
 FFMPEG.prototype.handleSnapshotRequest = function(request, callback) {
   let resolution = request.width + 'x' + request.height;
+   var vf = ['scale=' + request.width + ':' + request.height];
+  if(this.hflip) {
+    vf.push('hflip');
+  }
+  if(this.vflip) {
+    vf.push('vflip');
+  }
   var imageSource = this.ffmpegImageSource !== undefined ? this.ffmpegImageSource : this.ffmpegSource;
-  let ffmpeg = spawn(this.videoProcessor, (imageSource + ' -t 1 -s '+ resolution + ' -f image2 -').split(' '), {env: process.env});
+  let ffmpeg = spawn(this.videoProcessor, (imageSource + ' -t 1 -vf ' + vf.join(',') + ' -f image2 -').split(' '), {env: process.env});
   var imageBuffer = Buffer.alloc(0);
   this.log("Snapshot from " + this.name + " at " + resolution);
-  if(this.debug) console.log('ffmpeg '+imageSource + ' -t 1 -s '+ resolution + ' -f image2 -');
+  if(this.debug) console.log('ffmpeg '+imageSource + ' -t 1 -vf ' + vf.join(',') + ' -f image2 -');
   ffmpeg.stdout.on('data', function(data) {
     imageBuffer = Buffer.concat([imageBuffer, data]);
   });
