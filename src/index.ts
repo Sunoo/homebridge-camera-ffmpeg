5df50d60b9038c61c48dccffdd81fa31f30be128
Brandon McFarlin <brandonmcfarlin1016@gmail.com>
2020-05-22 18:03:46 -0400

    reorder initialization

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ import {
   HAP,
   Logging,
   PlatformAccessory,
+  PlatformAccessoryEvent,
   PlatformConfig,
 } from 'homebridge';
 import { StreamingDelegate } from './streamingDelegate';
@@ -40,8 +41,116 @@ class FfmpegPlatform implements DynamicPlatformPlugin {
     api.on(APIEvent.DID_FINISH_LAUNCHING, this.didFinishLaunching.bind(this));
   }
 
-  configureAccessory(accessory: PlatformAccessory): void {
-    // Won't be invoked
+  configureAccessory(cameraAccessory: PlatformAccessory): void {
+    this.log(`Configuring accessory ${cameraAccessory.displayName}`);
+
+    cameraAccessory.on(PlatformAccessoryEvent.IDENTIFY, () => {
+      this.log(`${cameraAccessory.displayName} identified!`);
+    });
+
+    const cameraConfig = cameraAccessory.context.cameraConfig;
+
+    if (cameraConfig.doorbell) {
+      const doorbellService = new hap.Service.Doorbell(`${cameraConfig.name} Doorbell`);
+      cameraAccessory.addService(doorbellService);
+      const switchService = new hap.Service.Switch(`${cameraConfig.name} Doorbell Trigger`, 'DoorbellTrigger');
+      switchService
+        .getCharacteristic(hap.Characteristic.On)
+        .on(CharacteristicEventTypes.SET, (state: CharacteristicValue, callback: CharacteristicSetCallback) => {
+          if (state) {
+            const doorbell = cameraAccessory.getService(hap.Service.Doorbell);
+            if (doorbell) {
+              doorbell.updateCharacteristic(
+                hap.Characteristic.ProgrammableSwitchEvent,
+                hap.Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS,
+              );
+
+              setTimeout(function () {
+                switchService.getCharacteristic(hap.Characteristic.On).updateValue(false);
+              }, 1000);
+            }
+          }
+          callback(null, state);
+        });
+
+      cameraAccessory.addService(switchService);
+    }
+    if (cameraConfig.motion) {
+      const button = new hap.Service.Switch(cameraConfig.name, 'MotionTrigger');
+      cameraAccessory.addService(button);
+
+      const motion = new hap.Service.MotionSensor(cameraConfig.name);
+      cameraAccessory.addService(motion);
+      const log = this.log;
+
+      button
+        .getCharacteristic(hap.Characteristic.On)
+        .on(CharacteristicEventTypes.SET, (on: CharacteristicValue, callback: CharacteristicSetCallback) => {
+          log.info(`Setting ${cameraAccessory.displayName} Motion to ${on}`);
+          const motionService = cameraAccessory.getService(hap.Service.MotionSensor);
+          if (motionService) {
+            motionService.setCharacteristic(hap.Characteristic.MotionDetected, on ? 1 : 0);
+            if (on) {
+              setTimeout(function () {
+                log.info(`Setting ${cameraAccessory.displayName} Button to false`);
+                const switchService = cameraAccessory.getService(hap.Service.Switch);
+                if (switchService) {
+                  switchService.setCharacteristic(hap.Characteristic.On, false);
+                }
+              }, 5000);
+            }
+          }
+          callback(null, on);
+        });
+    }
+
+    const streamingDelegate = new StreamingDelegate(hap, cameraConfig, this.log, this.config.videoProcessor);
+
+    const options: CameraControllerOptions = {
+      cameraStreamCount: cameraConfig.videoConfig.maxStreams || 2, // HomeKit requires at least 2 streams, but 1 is also just fine
+      delegate: streamingDelegate,
+      streamingOptions: {
+        supportedCryptoSuites: [hap.SRTPCryptoSuites.AES_CM_128_HMAC_SHA1_80],
+        video: {
+          resolutions: [
+            [320, 180, 30],
+            [320, 240, 15], // Apple Watch requires this configuration
+            [320, 240, 30],
+            [480, 270, 30],
+            [480, 360, 30],
+            [640, 360, 30],
+            [640, 480, 30],
+            [1280, 720, 30],
+            [1280, 960, 30],
+            [1920, 1080, 30],
+            [1600, 1200, 30],
+          ],
+          codec: {
+            profiles: [hap.H264Profile.BASELINE, hap.H264Profile.MAIN, hap.H264Profile.HIGH],
+            levels: [hap.H264Level.LEVEL3_1, hap.H264Level.LEVEL3_2, hap.H264Level.LEVEL4_0],
+          },
+        },
+        audio: {
+          codecs: [
+            {
+              type: AudioStreamingCodecType.OPUS,
+              samplerate: AudioStreamingSamplerate.KHZ_24,
+            },
+            {
+              type: AudioStreamingCodecType.AAC_ELD,
+              samplerate: AudioStreamingSamplerate.KHZ_16,
+            },
+          ],
+        },
+      },
+    };
+
+    const cameraController = new hap.CameraController(options);
+    streamingDelegate.controller = cameraController;
+
+    cameraAccessory.configureController(cameraController);
+
+    this.accessories.push(cameraAccessory);
   }
 
   didFinishLaunching(): void {
@@ -57,11 +166,9 @@ class FfmpegPlatform implements DynamicPlatformPlugin {
         }
         // Camera names must be unique
         const uuid = hap.uuid.generate(cameraName);
-        let accessoryType = hap.Accessory.Categories.CAMERA;
-        if (cameraConfig.doorbell) {
-          accessoryType = hap.Accessory.Categories.VIDEO_DOORBELL;
-        }
-        const cameraAccessory = new Accessory(cameraName, uuid, accessoryType);
+        cameraConfig.uuid = uuid;
+        const cameraAccessory = new Accessory(cameraName, uuid);
+        cameraAccessory.context.cameraConfig = cameraConfig;
         const cameraAccessoryInfo = cameraAccessory.getService(hap.Service.AccessoryInformation);
         if (cameraAccessoryInfo) {
           if (cameraConfig.manufacturer) {
@@ -78,110 +185,19 @@ class FfmpegPlatform implements DynamicPlatformPlugin {
           }
         }
 
-        if (cameraConfig.doorbell) {
-          const doorbellService = new hap.Service.Doorbell(`${cameraName} Doorbell`);
-          cameraAccessory.addService(doorbellService);
-          const switchService = new hap.Service.Switch(`${cameraName} Doorbell Trigger`, 'DoorbellTrigger');
-          switchService
-            .getCharacteristic(hap.Characteristic.On)
-            .on(CharacteristicEventTypes.SET, (state: CharacteristicValue, callback: CharacteristicSetCallback) => {
-              if (state) {
-                const doorbell = cameraAccessory.getService(hap.Service.Doorbell);
-                if (doorbell) {
-                  doorbell.updateCharacteristic(
-                    hap.Characteristic.ProgrammableSwitchEvent,
-                    hap.Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS,
-                  );
-
-                  setTimeout(function () {
-                    switchService.getCharacteristic(hap.Characteristic.On).updateValue(false);
-                  }, 1000);
-                }
-              }
-              callback(null, state);
-            });
-
-          cameraAccessory.addService(switchService);
+        // Only add new cameras that are not cached
+        if (!this.accessories.find((x: PlatformAccessory) => x.UUID === uuid)) {
+          this.configureAccessory(cameraAccessory); // abusing the configureAccessory here
+          this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [cameraAccessory]);
         }
-        if (cameraConfig.motion) {
-          const button = new hap.Service.Switch(cameraName, 'MotionTrigger');
-          cameraAccessory.addService(button);
-
-          const motion = new hap.Service.MotionSensor(cameraName);
-          cameraAccessory.addService(motion);
-          const log = this.log;
-
-          button
-            .getCharacteristic(hap.Characteristic.On)
-            .on(CharacteristicEventTypes.SET, (on: CharacteristicValue, callback: CharacteristicSetCallback) => {
-              log.info(`Setting ${cameraAccessory.displayName} Motion to ${on}`);
-              const motionService = cameraAccessory.getService(hap.Service.MotionSensor);
-              if (motionService) {
-                motionService.setCharacteristic(hap.Characteristic.MotionDetected, on ? 1 : 0);
-                if (on) {
-                  setTimeout(function () {
-                    log.info(`Setting ${cameraAccessory.displayName} Button to false`);
-                    const switchService = cameraAccessory.getService(hap.Service.Switch);
-                    if (switchService) {
-                      switchService.setCharacteristic(hap.Characteristic.On, false);
-                    }
-                  }, 5000);
-                }
-              }
-              callback(null, on);
-            });
-        }
-
-        const streamingDelegate = new StreamingDelegate(hap, cameraConfig, this.log, this.config.videoProcessor);
-
-        const options: CameraControllerOptions = {
-          cameraStreamCount: cameraConfig.videoConfig.maxStreams || 2, // HomeKit requires at least 2 streams, but 1 is also just fine
-          delegate: streamingDelegate,
-          streamingOptions: {
-            supportedCryptoSuites: [hap.SRTPCryptoSuites.AES_CM_128_HMAC_SHA1_80],
-            video: {
-              resolutions: [
-                [320, 180, 30],
-                [320, 240, 15], // Apple Watch requires this configuration
-                [320, 240, 30],
-                [480, 270, 30],
-                [480, 360, 30],
-                [640, 360, 30],
-                [640, 480, 30],
-                [1280, 720, 30],
-                [1280, 960, 30],
-                [1920, 1080, 30],
-                [1600, 1200, 30],
-              ],
-              codec: {
-                profiles: [hap.H264Profile.BASELINE, hap.H264Profile.MAIN, hap.H264Profile.HIGH],
-                levels: [hap.H264Level.LEVEL3_1, hap.H264Level.LEVEL3_2, hap.H264Level.LEVEL4_0],
-              },
-            },
-            audio: {
-              codecs: [
-                {
-                  type: AudioStreamingCodecType.OPUS,
-                  samplerate: AudioStreamingSamplerate.KHZ_24,
-                },
-                {
-                  type: AudioStreamingCodecType.AAC_ELD,
-                  samplerate: AudioStreamingSamplerate.KHZ_16,
-                },
-              ],
-            },
-          },
-        };
-
-        const cameraController = new hap.CameraController(options);
-        streamingDelegate.controller = cameraController;
-
-        cameraAccessory.configureController(cameraController);
-
-        this.accessories.push(cameraAccessory);
       });
 
-      this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, this.accessories);
+      // Remove cameras that were not in previous call
+      this.accessories.forEach((accessory: PlatformAccessory) => {
+        if (!cameras.find((x: any) => x.uuid === accessory.context.cameraConfig.uuid)) {
+          this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+        }
+      });
     }
   }
 }
