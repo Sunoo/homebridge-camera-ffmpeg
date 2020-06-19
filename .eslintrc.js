dd8cc6636240848eed82a777c55f168f39fd2c38
Donavan Becker <9875439+donavanbecker@users.noreply.github.com>
2020-06-19 14:42:27 -0500

    v2.0.0

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/array-type': [
+      'error',
+      { default: 'generic' }
+    ],
+    '@typescript-eslint/camelcase': ['off'],
+    '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/no-this-alias': ['off'],
+  },
+}
